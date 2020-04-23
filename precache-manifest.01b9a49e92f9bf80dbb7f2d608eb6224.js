161530fcda2e0e197bd75aa2f516658aa8654433
clinedim <marc.clinedinst@gmail.com>
2020-04-23 17:49:58 -0400

    Updates

---
+++ precache-manifest.01b9a49e92f9bf80dbb7f2d608eb6224.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7edcf247a73848705e0e8944673c34fa",
+    "url": "/ecommerce-site/index.html"
+  },
+  {
+    "revision": "ef68e36091dd21fde1ff",
+    "url": "/ecommerce-site/static/css/main.2c5bdb88.chunk.css"
+  },
+  {
+    "revision": "d596eb84f36e28349ca7",
+    "url": "/ecommerce-site/static/js/2.7d00ec29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ecommerce-site/static/js/2.7d00ec29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef68e36091dd21fde1ff",
+    "url": "/ecommerce-site/static/js/main.cb40ae32.chunk.js"
+  },
+  {
+    "revision": "94df8540c6d4be8ea23e",
+    "url": "/ecommerce-site/static/js/runtime-main.64d1861f.js"
+  }
+]);
